e3a94ae5c4db0850cfc256e1585afe29239810c1
kuanghc1 <kuanghc1@lenovo.com>
2022-12-14 14:32:51 +0800

    add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+Add new features:
+
+- Support Kotlin based Spring Boot project which uses @Scheduled annotation
+
+## [Unreleased]
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.date
+
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.10.0"
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
 group = "com.khch"
@@ -27,5 +32,35 @@ tasks {
         version.set("${project.version}")
         sinceBuild.set("193.7288.26")
         untilBuild.set("223.*")
+
+        changeNotes.set(provider {
+            changelog.renderItem(
+                changelog
+                    .getUnreleased()
+                    .withHeader(false)
+                    .withEmptySections(false),
+                Changelog.OutputType.HTML
+            )
+        })
     }
+}
+
+changelog {
+    version.set("1.0.1")
+    path.set(file("CHANGELOG.md").canonicalPath)
+    header.set(provider { "[${version.get()}] - ${date()}" })
+    headerParserRegex.set("""(\d+\.\d+)""".toRegex())
+    introduction.set(
+        """
+        Add new features:
+        
+        - Support Kotlin based Spring Boot project which uses @Scheduled annotation
+        """.trimIndent()
+    )
+    itemPrefix.set("-")
+    keepUnreleasedSection.set(false)
+    unreleasedTerm.set("[Unreleased]")
+    groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+    lineSeparator.set("\n")
+    combinePreReleases.set(true)
 }
